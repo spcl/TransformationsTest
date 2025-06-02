0b976b4f2bb97be6f9b2d5182dc2a0c8551b5fc3
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-06-02 11:17:45 +0200

    Fix empty transformations syntax errors

--- empty_transformation.py
+++ empty_transformation.py
@@ -18,24 +18,4 @@ class EmptyTransformation(transformation.MultiStateTransformation):
         return True
 
     def apply(self, graph: ControlFlowRegion, sdfg: dace.SDFG):
-        returnimport dace
-        from dace.sdfg import utils as sdutil
-        from dace.sdfg.state import ControlFlowRegion
-        from dace.transformation import transformation
-
-
-        @transformation.explicit_cf_compatible
-        class EmptyTransformation(transformation.MultiStateTransformation):
-                @staticmethod
-                    def annotates_memlets():
-                                return False
-
-                                @classmethod
-                                    def expressions(cls):
-                                                return [sdutil.node_path_graph(cls.map_entry)]
-
-                                                def can_be_applied(self, graph: ControlFlowRegion, expr_index, sdfg: dace.SDFG, permissive=False):
-                                                            return True
-
-                                                            def apply(self, graph: ControlFlowRegion, sdfg: dace.SDFG):
-                                                                        return
+        return
